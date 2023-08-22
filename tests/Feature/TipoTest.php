d5045568ab12d3e9c73942faa3d19107f7720e41
Lehgolaz <lehgolaz66rodrigues@email.com>
2023-08-21 21:09:37 -0300

    Tipo teste

---
+++ tests/Feature/TipoTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Tipo;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TipoTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+    /**
+     * A basic feature test example.
+     * A função index deve retornar 5 cadastros
+     * @return void
+     */
+    public function test_funcao_index_retornar_array_com_sucesso()
+    {
+       //Criar paramentros
+       $tipos = Tipo::factory()->count(5)->create();
+       dd($tipos);
+       //Processar
+
+       //Verificar resposta
+    }
+}
