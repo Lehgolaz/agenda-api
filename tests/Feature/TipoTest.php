bbeb517937f795aa472a216f04deb95c9e4b57d2
Lehgolaz <lehgolaz66rodrigues@email.com>
2023-08-21 21:40:08 -0300

    Teste tipo

--- tests/Feature/TipoTest.php
+++ tests/Feature/TipoTest.php
@@ -10,33 +10,32 @@ use Tests\TestCase;
 class TipoTest extends TestCase
 {
     use RefreshDatabase, WithFaker;
+
     /**
-     * A basic feature test example.
      * A função index deve retornar 5 cadastros
+     *
      * @return void
      */
     public function test_funcao_index_retornar_array_com_sucesso()
     {
-        //Criar paramentros
-        $tipos = Tipo::factory()->count(5)->create();
-        dd($tipos);
+        //Criar parametros
+        Tipo::factory()->count(5)->create();
         //Processar
-        //fazer uma chamada para  arota index no api
-        //usar verbo GET
-        $response = $this->getJson('/api/tipos');
+        //Fazer uma chamada para a rota index no api
+        //Usar verbo GET
+        $response = $this->getJson('/api/tipos/');
 
-        dd($response['data']);
+        // dd($response['data']);
 
         //Verificar resposta
         $response
             ->assertStatus(200)
-            ->assertJsonCount(5, 'data')
-            ->assertJsonStructure(
-                [
-                    'data' => [
-                        '*' => ['id', 'descricao' . 'created_at', 'update_at']
-                    ]
+            ->assertJsonCount(5,'data')
+            ->assertJsonStructure([
+                'data' =>[
+                    '*' =>['id','descricao','created_at','update_at']
                 ]
+            ]
             );
     }
 }
