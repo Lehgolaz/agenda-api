a75180c29deae2db0476a94a5de218c9d12824f1
Lehgolaz <lehgolaz66rodrigues@email.com>
2023-08-28 21:57:03 -0300

    Teste

--- database/migrations/2023_08_25_235033_create_tarefs_table.php
+++ database/migrations/2023_08_25_235033_create_tarefs_table.php
@@ -15,7 +15,13 @@ return new class extends Migration
     {
         Schema::create('tarefs', function (Blueprint $table) {
             $table->id();
-            $table->string('descricao')->unique();
+            $table->dateTime('data');
+            $table->string('descricao');
+            $table->text('descricao');
+            $table->string('contato');
+            $table->unsignedBigInteger('tipo_id');
+            //$table->unsignedBigInteger('fk_id');
+            $table->foreign('tipo_id')->references('id')->on('tipos');
             $table->timestamps();
         });
     }
